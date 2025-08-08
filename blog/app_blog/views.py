97ff577d64c0a4b7d12f34ead318bd6ea79c0c36
Akbar <akbarmansourifar8200@gmail.com>
2025-08-08 11:50:24 +0330

    add post creation API

--- blog/app_blog/views.py
+++ blog/app_blog/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostListAPIView(APIView):
+    def get(self, request):
+        posts = Post.objects.all().order_by('-created_at')
+        serializer = PostSerializer(posts, many=True) 
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+class PostDetailAPIView(APIView):
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    
+    
